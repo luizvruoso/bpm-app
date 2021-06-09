e9105f166cd3f8730bf01377455ead7828900139
Luiz Vinícius Ruoso <53343378+luizvruoso@users.noreply.github.com>
2021-06-08 23:18:00 -0300

    Dashboard

--- webapp/src/lib/screens/Dashboard.js
+++ webapp/src/lib/screens/Dashboard.js
@@ -1,37 +1,29 @@
-import React, {Component} from 'react';
-import Card from '../components/Card/index';
+import React, { Component } from "react";
+import Card from "../components/Card/index";
+import Header from "../components/Header/index";
 
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+  }
 
- class Dashboard extends Component {
-    constructor(props) {
-      super(props);
-        
-    }
-  
-    render() {
-      return (
-
-        <div className="col-10 containerDash">
-          <div className="row">
-                <div className="col-12">
-                      <p>Dashboard</p>
-
-                  </div>
-            </div>
-
-            <div className="row">
-                <div className="col-12">
-                    <Card/>
-                </div>
-                <div className="col-12">
-                    <Card/>
-                </div>
-             </div>
+  render() {
+    return (
+      <div className="col-10 containerDash" style={{height: '100vh', overflowX: 'auto'}} >
+        <div className="row">
+          <Header content={"Dashboard"} />
         </div>
 
-);
-    }
+        <div className="row" >
+          
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+      </div>
+    );
   }
-  
-  
-  export default Dashboard;
+}
+
+export default Dashboard;
