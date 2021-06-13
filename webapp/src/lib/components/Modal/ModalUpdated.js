1d4428e22e809e249fdc9004feb1c84e34c3d89e
VictorSoldera <victorsolderalf@gmail.com>
2021-06-13 18:45:13 -0300

    Luiz, me deixa em paz. Obrigado!

--- webapp/src/lib/components/Modal/ModalUpdated.js
+++ webapp/src/lib/components/Modal/ModalUpdated.js
@@ -9,15 +9,15 @@ export default function TestModal(props) {
     <Modal>
 
       <ModalHeader>
-        <h3 style={{color: "#053245", margin: "auto", marginTop: "127px"}}>Incluir Dependente</h3>
+        <h3 style={{color: "#053245", margin: "auto", marginTop: "90px", font: "Roboto", fontWeight: "700"}}>Incluir Dependente</h3>
       </ModalHeader>
 
       <ModalBody>
-        <center><input style={{ width:"436px", borderRadius: "5px", border: "1px solid #BCBCBC", boxSizing: "border-box", marginTop: "15px"}} placeholder={"Token de Cadastro"}></input></center>
+        <center><input style={{width:"436px", height: "44px", borderRadius: "5px", border: "1px solid #BCBCBC", boxSizing: "border-box", marginTop: "15px", font: "Roboto", paddingLeft: "15px"}} placeholder={"Token de Cadastro"}></input></center>
       </ModalBody>
 
       <ModalFooter>
-        <button onClick={ props.close } style = {{margin: "auto", width: "243px",height: "44px",background: "#6E6E6E", border: "1px solid #BCBCBC", boxSizing: "border-box", borderRadius: "5px", marginBottom: "133px"}}><span style = {{color: "white"}}>Salvar</span></button>
+        <button onClick={ props.close } style = {{margin: "auto", width: "243px", height: "44px",background: "#6E6E6E", border: "1px solid #BCBCBC", boxSizing: "border-box", borderRadius: "5px", marginBottom: "133px", marginTop: "15px"}}><span style = {{color: "white", font: "Roboto", fontSize: "20px", fontWeight: "400"}}>Salvar</span></button>
       </ModalFooter>
 
     </Modal>
