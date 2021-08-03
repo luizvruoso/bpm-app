c4630dbc6a668ae91b1464b0b50464af6fbf13e8
Victor Soldera <victorsolderalf@gmail.com>
2021-08-02 22:05:32 -0300

    feat: adding post and get methods

---
+++ bpmApi/src/main/java/com/bpmapi/service/mapper/UserService.java
@@ -0,0 +1,8 @@
+package com.bpmapi.service.mapper;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
--- bpmApi/src/test/java/com/bpmapi/BpmApiApplicationTests.java
+++ bpmApi/src/test/java/com/bpmapi/BpmApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.bpmapi;
+package com.bpmapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
